e78d8ca5ad9f7681c0bf20248545235083747c45
Joe <132949140+JoeMonnig@users.noreply.github.com>
2026-05-27 15:33:18 -0500

    Remove preload.js and its exposed API
    Removed unused file

--- code/preload.js
+++ code/preload.js
@@ -1,7 +1,1 @@
-const { contextBridge, ipcRenderer } = require('electron');
 
-contextBridge.exposeInMainWorld('secureAPI', {
-  attemptLogin: (username, password) => ipcRenderer.invoke('attempt-login', username, password),
-  attemptRegister: (username, password) => ipcRenderer.invoke('attempt-register', username, password),
-  getUserInfo: () => ipcRenderer.invoke('get-user-info')
-});
